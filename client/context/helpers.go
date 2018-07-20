5dc344c2d3ca71e7f810a015e121ab0279160b9d
xujiacheng <xujiacheng28@outlook.com>
2018-07-20 10:35:24 +0800

    update the develop

--- client/context/helpers.go
+++ client/context/helpers.go
@@ -277,6 +277,7 @@ func (ctx CoreContext) EnsureSignBuildBroadcast(name string, msgs []sdk.Msg, cdc
 	}
 	if ctx.PrintResponse {
 		fmt.Printf("Committed at block %d. Hash: %s Response:%+v \n", res.Height, res.Hash.String(), res.DeliverTx)
+
 	} else {
 		fmt.Printf("Committed at block %d. Hash: %s \n", res.Height, res.Hash.String())
 	}
